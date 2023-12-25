ba4ad3cd16ede7761534d6810e863ffc38276123
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-12-25 22:26:07 +0545

    useWarmUpBrowser added and logout login with clerk implemented

--- app/(modals)/login.tsx
+++ app/(modals)/login.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { useWarmUpBrowser } from "@/hooks/use-warm-up-browser";
 import React from "react";
 
 const Page = () => {
+  useWarmUpBrowser();
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Login</Text>
     </View>
   );
 };
 
 export default Page;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 24,
+  },
+});
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Button } from "react-native";
+import { useAuth } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import Colors from "@/constants/Colors";
 import React from "react";
 
 const Page = () => {
+  const { isSignedIn, signOut } = useAuth();
   return (
     <View>
-      <Text>Profile</Text>
+      {isSignedIn && (
+        <Button title="Log Out" onPress={() => signOut()} color={Colors.dark} />
+      )}
+      {!isSignedIn && (
+        <Link href={"/login"} asChild>
+          <Button title="Log In" color={Colors.dark} />
+        </Link>
+      )}
     </View>
   );
 };
